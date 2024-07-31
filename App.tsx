62edfd288a6ca9b3e0101a66257b72de1a6bea7e
Rushikesh Jagdevrao Bhumkar <94697002+Rushibhumkar@users.noreply.github.com>
2024-07-31 21:17:31 +0530

    lms app created

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import {View, Text, StatusBar} from 'react-native';
+import {View, Text, StatusBar, Platform} from 'react-native';
 import React from 'react';
 
 const App = () => {
+  const statusbarHeight = StatusBar.currentHeight;
+  console.log({statusbarHeight});
   return (
-    <View style={{flex: 1, backgroundColor: '#fff'}}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Platform.OS === 'ios' ? 12 : statusbarHeight,
+      }}>
       <StatusBar
-        translucent
+        translucent={true}
         backgroundColor={'#fff'}
         barStyle={'dark-content'}
       />
